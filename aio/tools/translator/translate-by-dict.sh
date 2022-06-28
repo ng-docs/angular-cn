e62f577b2107a40afcbc2854624e56d92dc1b7ad
Zhicheng WANG <zcwang@thoughtworks.com>
2022-06-28 14:00:08 +0800

    fix: 修正翻译脚本

--- aio/tools/translator/translate-by-dict.sh
+++ aio/tools/translator/translate-by-dict.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env sh
 
 # markdown 文档
-nt translate content/\(cli\|errors\|guide\|start\|tutorial\|special-elements\)/*.md --engine=dict --dict ./tools/translator/dict/angular
+nt translate content/\(cli\|errors\|guide\|start\|tutorial\|special-elements\|extended-diagnostics\)/*.md --engine=dict --dict ./tools/translator/dict/angular
 
 # html 文档
-nt translate content/**/*.html --engine=dict --dict ./tools/translator/dict/angular
+nt translate content/**/*.html '!content/examples/**/*.html' --engine=dict --dict ./tools/translator/dict/angular
 
 # 导航菜单
-nt translate content/navigation.json --engine=dict --dict ./tools/translator/dict/angular
+nt translate content/navigation.json --engine=dict --dict ./tools/translator/dict/angular --jsonProperties=title --jsonProperties=tooltip
 
+# 社区资源
+nt translate content/marketing/resources.json --engine=dict --dict ./tools/translator/dict/angular --jsonProperties=title --jsonProperties=desc
+
+# 帮助文件
+nt translate content/cli-src/help/*.json --engine=dict --dict ./tools/translator/dict/angular --jsonProperties=description --jsonProperties=shortDescription --jsonProperties=longDescription
+
+cd ..
 # 源码
-nt translate '../packages/**/*.ts' '!../packages/**/*.d.ts' '!../packages/**/*_spec.ts' '!../packages/**/*.spec.ts' -et internal --engine=dict --dict ./tools/translator/dict/angular
+nt translate 'packages/**/*.ts' '!packages/**/*.d.ts' '!packages/**/*_spec.ts' '!packages/**/*.spec.ts' -et internal -et nodoc --engine=dict --dict ./tools/translator/dict/angular
+cd -
--- aio/tools/translator/translate-by-gcloud.sh
+++ aio/tools/translator/translate-by-gcloud.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env sh
 
+# angular 模型已过期，暂用 spring 模型
+
 # markdown 文档
-nt translate content/\(cli\|errors\|guide\|start\|tutorial\|special-elements\)/*.md --engine=gcloud --domain=angular
+nt translate content/\(cli\|errors\|guide\|start\|tutorial\|special-elements\|extended-diagnostics\)/*.md --engine=gcloud --domain=spring
 
 # html 文档
-nt translate content/**/*.html --engine=gcloud --domain=angular
+nt translate content/**/*.html '!content/examples/**/*.html' --engine=gcloud --domain=spring
 
 # 导航菜单
-nt translate content/navigation.json --engine=gcloud --domain=angular
+nt translate content/navigation.json --engine=gcloud --domain=spring --jsonProperties=title --jsonProperties=tooltip
 
+# 资源列表
+nt translate content/marketing/resources.json --engine=gcloud --domain=spring --jsonProperties=title --jsonProperties=desc
+
+# 帮助文件
+nt translate content/cli-src/help/*.json --engine=gcloud --domain=spring --jsonProperties=description --jsonProperties=shortDescription --jsonProperties=longDescription
+
+cd ..
 # 源码
-nt translate '../packages/**/*.ts' '!../packages/**/*.d.ts' '!../packages/**/*_spec.ts' '!../packages/**/*.spec.ts' -et internal  --engine=gcloud --domain=angular
+nt translate 'packages/**/*.ts' '!packages/**/*.d.ts' '!packages/**/*_spec.ts' '!packages/**/*.spec.ts' -et internal -et=nodoc --engine=gcloud --domain=spring
+
+cd -
