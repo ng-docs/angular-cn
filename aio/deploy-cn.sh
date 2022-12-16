5a5936cd7f937c37b116261d0138909d2575fc24
Zhicheng WANG <zcwang@thoughtworks.com>
2022-12-17 07:43:55 +0800

    fix: 修正部署脚本，确保所有文件可写

--- aio/deploy-cn.sh
+++ aio/deploy-cn.sh
@@ -6,7 +6,7 @@ set -e
 commitSha=$(git rev-parse --short HEAD)
 commitMessage=$(git log --oneline -n 1)
 
-npm i -g @awesome-fe/translate@1.1.21
+#npm i -g @awesome-fe/translate@1.1.21
 
 cd $(dirname $0)
 
@@ -16,11 +16,11 @@ cd ./release/latest.angular.live
 
 git pull -r
 
+chmod -R +w .
+
 cp -r ../../../dist/bin/aio/build/* .
 
-chmod -R +w ./generated/**/*.json
-
-nt mark ./generated/**/*.json
+nt mark './generated/**/*.json'
 
 cp index.html 404.html
 
